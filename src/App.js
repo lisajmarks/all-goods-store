b5513a73d6a281090a1682252192342e38e08eec
Lisa <lisajmarks@pm.me>
2021-09-05 10:25:52 -0700

    add shop components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Default from './components/Default'
+import Product from './components/Product'
+import ProductList from './components/ProductList'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar></Navbar>
+      <Cart></Cart>
+      <Default></Default>
+      <Product></Product>
+      <ProductList></ProductList>
     </div>
   );
 }
