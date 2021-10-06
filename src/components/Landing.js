fe41bc396e8cfae3be253ec2631af654d4e229b3
Lisa <lisajmarks@pm.me>
2021-10-06 16:00:26 -0700

    fix image on landing for mobile

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../styles/Landing.css";
-import { Container, Image, Jumbotron, Row } from 'react-bootstrap';
+import { Container, Image, Row } from 'react-bootstrap';
 import shopping from '../images/shopping.png'
 import banner from '../images/banner.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -9,9 +9,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const Landing = () => { 
     return (
     <div> 
-        <Jumbotron className="jumbo">
-            <Image src={banner} />
-        </Jumbotron>
+        {/* <Jumbotron className="jumbo">
+            <Image src={banner} style={{ display: `flex`}} />
+        </Jumbotron> */}
+        <Container fluid className="jumbo px-0">
+            <Image src={banner} className="img-fluid"/>
+        </Container> 
             <Container fluid className="landingpage px-0"> 
                 <Row> 
                 <Image className="shopping-img" fluid src={shopping} />
